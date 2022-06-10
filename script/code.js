4465eb3a69c7fdf95717816a6a9316f9b93a100e
DanielJamesF <fredericksd17@gmail.com>
2022-06-10 14:09:41 +0200

    changes

--- script/code.js
+++ script/code.js
@@ -1,8 +1,8 @@
 function smartPeople() {
     let names = ['Charles', 'Amanda', 'Bulelani', 'Cameron', 'Cassidy', 'Clayton', 'Daniel', 'Emile', 'Ikhlaas', 'Joshe', 'Joshua', 'Liam', 'Marshalino', 'Meagan', 'Michaela', 'Mikhail', 'Monique', 'Philani', 'Ra\'ees', 'Rasheedah', 'Reagan', 'Ridhaa', 'Saleem', 'Shane', 'Sibongile', 'Siliziwe', 'Sinethemba', 'Taucia', 'Yaseen'
 ];
-let ul = document.createElement('ul');
-document.body.appendChild(ul);
+let Newul = document.createElement('ul');
+document.body.appendChild(Newul);
 
 names.forEach( (item, index)=> {
     ul.innerHTML += `<li>${index}: ${item} </li>`;
