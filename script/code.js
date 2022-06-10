fbe8b1f96b09d5a7de6dc32346a3898a6c26d0dc
DanielJamesF <fredericksd17@gmail.com>
2022-06-10 14:08:39 +0200

    changes

--- script/code.js
+++ script/code.js
@@ -1,5 +1,12 @@
-let smartPeople = ['Charles', 'Amanda', 'Bulelani', 'Cameron', 'Cassidy', 'Clayton', 'Daniel', 'Emile', 'Ikhlaas', 'Joshe', 'Joshua', 'Liam', 'Marshalino', 'Meagan', 'Michaela', 'Mikhail', 'Monique', 'Philani', 'Ra\'ees', 'Rasheedah', 'Reagan', 'Ridhaa', 'Saleem', 'Shane', 'Sibongile', 'Siliziwe', 'Sinethemba', 'Taucia', 'Yaseen'];
-let wrapper = document.getElementById('names');
-smartPeople.forEach((item)=>{
-    wrapper.innerHTML += `<li>${item}</li>`
-} );
+function smartPeople() {
+    let names = ['Charles', 'Amanda', 'Bulelani', 'Cameron', 'Cassidy', 'Clayton', 'Daniel', 'Emile', 'Ikhlaas', 'Joshe', 'Joshua', 'Liam', 'Marshalino', 'Meagan', 'Michaela', 'Mikhail', 'Monique', 'Philani', 'Ra\'ees', 'Rasheedah', 'Reagan', 'Ridhaa', 'Saleem', 'Shane', 'Sibongile', 'Siliziwe', 'Sinethemba', 'Taucia', 'Yaseen'
+];
+let ul = document.createElement('ul');
+document.body.appendChild(ul);
+
+names.forEach( (item, index)=> {
+    ul.innerHTML += `<li>${index}: ${item} </li>`;
+});
+}
+
+smartPeople();
